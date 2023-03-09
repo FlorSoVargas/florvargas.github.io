1e8cb7bee8d2d7e4f59d1f8ab60c41ea436e167a
Florencia Vargas <florenciavargas567@gmail.com>
2023-03-08 23:38:29 -0300

    trabajoCV_basico_terminado

---
+++ cv.js
@@ -0,0 +1,13 @@
+let negro=false;
+document.getElementById("boton").onclick= 
+function()
+{
+    if (negro){
+        negro=false
+        document.body.style.backgroundColor='#FFFFFF'
+        
+    }
+    else{document.body.style.backgroundColor='#000000';
+    negro= true;}
+
+}
